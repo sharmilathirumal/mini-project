cb4c352ab2b6e1be8cbdbd2d42e60c35826e1b3a
sharmilathirumal <154589212+sharmilathirumal@users.noreply.github.com>
2025-04-07 06:54:25 +0530

    Update AddressRepository.java

--- spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/AddressRepository.java
+++ spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/AddressRepository.java
@@ -4,6 +4,7 @@ import com.example.spring_thymeleaf.entity.Address;
 import com.example.spring_thymeleaf.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address,Long> {
     //Address findByEmployee_Id(Long id);
 }
