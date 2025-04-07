8dc25a46a9fc67d655ce0613b6aa624b015249e7
sharmilathirumal <154589212+sharmilathirumal@users.noreply.github.com>
2025-04-07 07:26:56 +0530

    Update TeamRepository.java

--- spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/TeamRepository.java
+++ spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/TeamRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 
-@Repository
 public interface TeamRepository extends JpaRepository<Team,Long> {
     boolean existsByName(String name);
     List<Team> findByDepartment_Id(Long id);
