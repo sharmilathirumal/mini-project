081f5b114d2f8510d58dc4201d1a83169970552e
sharmilathirumal <154589212+sharmilathirumal@users.noreply.github.com>
2025-04-07 06:56:15 +0530

    Update DepartmentRepository.java

--- spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/DepartmentRepository.java
+++ spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/DepartmentRepository.java
@@ -2,9 +2,9 @@ package com.example.spring_thymeleaf.repository;
 
 import com.example.spring_thymeleaf.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
     boolean existsByDepartmentName(String departmentName);
 }
