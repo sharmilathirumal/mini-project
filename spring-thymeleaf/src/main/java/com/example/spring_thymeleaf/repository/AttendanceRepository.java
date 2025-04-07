0943ebfb070fb228e684ab0d74e737c48acfa22b
sharmilathirumal <154589212+sharmilathirumal@users.noreply.github.com>
2025-04-07 06:55:50 +0530

    Update AttendanceRepository.java

--- spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/AttendanceRepository.java
+++ spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/AttendanceRepository.java
@@ -3,17 +3,16 @@ package com.example.spring_thymeleaf.repository;
 import com.example.spring_thymeleaf.dto.AttendanceDTO;
 import com.example.spring_thymeleaf.entity.Attendance;
 import com.example.spring_thymeleaf.entity.Employee;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
     public List<Attendance> findByEmployee_Id(Long id);
     @Query("SELECT a.date FROM Attendance a WHERE a.employee.id = :employeeId ORDER BY a.date ASC")
