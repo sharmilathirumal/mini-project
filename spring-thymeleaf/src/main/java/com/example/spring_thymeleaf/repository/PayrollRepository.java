3535454a1a6719f19d08267ab4078cfd82e7b1bc
sharmilathirumal <154589212+sharmilathirumal@users.noreply.github.com>
2025-04-07 06:57:52 +0530

    Update PayrollRepository.java

--- spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/PayrollRepository.java
+++ spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/PayrollRepository.java
@@ -5,9 +5,9 @@ import com.example.spring_thymeleaf.entity.Payroll;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
+@Repository
 public interface PayrollRepository extends JpaRepository<Payroll,Long> {
      List<Payroll> findByEmployeeId(Long id);
      @Query("SELECT COUNT(p) FROM Payroll p WHERE p.employee=:employee AND FUNCTION('YEAR', p.payDate) = :year AND FUNCTION('MONTH', p.payDate) = :month")
