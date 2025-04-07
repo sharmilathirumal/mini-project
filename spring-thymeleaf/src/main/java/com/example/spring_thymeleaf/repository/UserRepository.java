9e9d0b0c0b7d845121d85462ba4a7b733b3db729
sharmilathirumal <154589212+sharmilathirumal@users.noreply.github.com>
2025-04-07 06:58:54 +0530

    Update UserRepository.java

--- spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/UserRepository.java
+++ spring-thymeleaf/src/main/java/com/example/spring_thymeleaf/repository/UserRepository.java
@@ -2,9 +2,9 @@ package com.example.spring_thymeleaf.repository;
 
 import com.example.spring_thymeleaf.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     boolean existsByUserName(String userName);
     boolean existsByEmployeeId(Long id);
